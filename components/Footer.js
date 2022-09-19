2a4e9933315b19f96ac012de5c550f6d51c8ceb9
Nicolas  Baucheron <nicolas@Nicolass-MacBook-Air.local>
2022-09-20 00:24:14 +0200

    Added Footer + edit transitions

--- components/Footer.js
+++ components/Footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
   return (
     <FooterStyle
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 1.5 } }}
+      animate={{ opacity: 1, transition: { duration: 1.5, delay: 2 } }}
     >
       <FooterStyleInner>
         <h1>LIGHTHOUSE</h1>
