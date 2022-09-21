5bc21db5bb1c6e72fe4fea497ee4a31c44384425
Nicolas  Baucheron <nicolas@Nicolass-MacBook-Air.local>
2022-09-22 00:35:23 +0200

    new

--- components/Tracklist.js
+++ components/Tracklist.js
@@ -1,38 +1,37 @@
 import Link from "next/link";
 import { TracklistStyled } from "../styles/ProductDetails";
 import { FaYoutube } from "react-icons/fa";
-
+import { v4 as uuidv4 } from "uuid";
 export default function Tracklist({ tracklist }) {
   return (
     <>
       <TracklistStyled>
         <h3>Tracklist :</h3>
 
-        {tracklist.map((trackItem, k) => (
-          <>
-            {trackItem.SIDE && (
-              <p key={k}>{trackItem.SIDE.replace("_", " ")}</p>
-            )}
-            {trackItem.tracklist2.map((trackItem2, i) => (
-              <ul>
-                <li key={i}>
-                  <div>
-                    <span>
+        {tracklist.map((trackItem) => (
+          <div key={uuidv4()}>
+            <p key={uuidv4()}>{trackItem.SIDE.replace("_", " ")}</p>
+
+            <ul key={uuidv4()}>
+              {trackItem.tracklist2.map((trackItem2, i) => (
+                <li key={uuidv4()}>
+                  <div key={uuidv4()}>
+                    <span key={uuidv4()}>
                       {trackItem.SIDE.replace("SIDE_", " ") + (i + 1)}
                     </span>
-                    <span>{trackItem2.track}</span>
+                    <span key={uuidv4()}>{trackItem2.track}</span>
                   </div>
                   {trackItem2.video && (
-                    <Link href={trackItem2.video}>
-                      <span>
+                    <Link key={uuidv4()} href={trackItem2.video}>
+                      <span key={uuidv4()}>
                         <FaYoutube />
                       </span>
                     </Link>
                   )}
                 </li>
-              </ul>
-            ))}
-          </>
+              ))}
+            </ul>
+          </div>
         ))}
       </TracklistStyled>
     </>
